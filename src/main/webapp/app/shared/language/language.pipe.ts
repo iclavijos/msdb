4180a69a4c0111f56da467dd1d787fe209605574
Ivan Clavijos <iclavijos@gmail.com>
2017-06-24 08:09:10 +0200

    Undone language folders renaming

--- src/main/webapp/app/shared/language/language.pipe.ts
+++ src/main/webapp/app/shared/language/language.pipe.ts
@@ -3,10 +3,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'findLanguageFromKey'})
 export class FindLanguageFromKeyPipe implements PipeTransform {
     private languages: any = {
-        'ca-ES': 'Català',
-        'en-EN': 'English',
-        'es-ES': 'Español',
-        'gl-ES': 'Galego',
+        'ca': 'Català',
+        'en': 'English',
+        'es': 'Español',
+        'gl': 'Galego',
     };
     transform(lang: string): string {
         return this.languages[lang];
