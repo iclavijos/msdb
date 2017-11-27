c03a85d92f203e88cb93a4eeb7cf096942a2f1b4
Ivan Clavijos <iclavijos@gmail.com>
2017-11-27 19:06:23 +0100

    MongoDB configuration

--- src/main/java/com/icesoft/msdb/config/DatabaseConfiguration.java
+++ src/main/java/com/icesoft/msdb/config/DatabaseConfiguration.java
@@ -1,26 +1,27 @@
 package com.icesoft.msdb.config;
 
-import io.github.jhipster.config.JHipsterConstants;
-import io.github.jhipster.config.liquibase.AsyncSpringLiquibase;
+import java.sql.SQLException;
 
 import org.h2.tools.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.sql.DataSource;
-import java.sql.SQLException;
+import io.github.jhipster.config.JHipsterConstants;
 
 @Configuration
 @EnableJpaRepositories("com.icesoft.msdb.repository")
@@ -32,6 +33,9 @@ public class DatabaseConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
 
+    @Autowired
+    private MongoMappingContext mongoMappingContext;
+    
     private final Environment env;
 
     public DatabaseConfiguration(Environment env) {
@@ -50,23 +54,12 @@ public class DatabaseConfiguration {
         return Server.createTcpServer("-tcp","-tcpAllowOthers");
     }
 
-//    @Bean
-//    public SpringLiquibase liquibase(@Qualifier("taskExecutor") TaskExecutor taskExecutor,
-//            DataSource dataSource, LiquibaseProperties liquibaseProperties) {
-//
-//        // Use liquibase.integration.spring.SpringLiquibase if you don't want Liquibase to start asynchronously
-//        SpringLiquibase liquibase = new AsyncSpringLiquibase(taskExecutor, env);
-//        liquibase.setDataSource(dataSource);
-//        liquibase.setChangeLog("classpath:config/liquibase/master.xml");
-//        liquibase.setContexts(liquibaseProperties.getContexts());
-//        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
-//        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
-//        if (env.acceptsProfiles(JHipsterConstants.SPRING_PROFILE_NO_LIQUIBASE)) {
-//            liquibase.setShouldRun(false);
-//        } else {
-//            liquibase.setShouldRun(liquibaseProperties.isEnabled());
-//            log.debug("Configuring Liquibase");
-//        }
-//        return liquibase;
-//    }
+    @Bean
+    public MappingMongoConverter mongoConverter(MongoDbFactory mongoFactory) throws Exception {
+      DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoFactory);
+      MappingMongoConverter mongoConverter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
+      mongoConverter.setMapKeyDotReplacement("_");
+
+      return mongoConverter;
+    }
 }
