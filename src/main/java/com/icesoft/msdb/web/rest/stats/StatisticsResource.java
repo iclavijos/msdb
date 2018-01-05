7cfd7cc5078af583aa9a486af47356dc1afda628
Ivan Clavijos <iclavijos@gmail.com>
2018-01-05 09:22:27 +0100

    Proper updating on championship statistics when doing a full rebuild

--- src/main/java/com/icesoft/msdb/web/rest/stats/StatisticsResource.java
+++ src/main/java/com/icesoft/msdb/web/rest/stats/StatisticsResource.java
@@ -1,6 +1,8 @@
 package com.icesoft.msdb.web.rest.stats;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.icesoft.msdb.repository.EventEditionRepository;
+import com.icesoft.msdb.repository.SeriesEditionRepository;
 import com.icesoft.msdb.security.AuthoritiesConstants;
+import com.icesoft.msdb.service.SeriesEditionService;
 import com.icesoft.msdb.service.StatisticsService;
 import com.icesoft.msdb.web.rest.util.HeaderUtil;
 
@@ -23,10 +27,15 @@ public class StatisticsResource {
 	private final Logger log = LoggerFactory.getLogger(StatisticsResource.class);
 	
 	private final EventEditionRepository eventEditionRepo;
+	private final SeriesEditionRepository seriesEditionRepo;
+	private final SeriesEditionService seriesEditionService;
 	private final StatisticsService statsService;
 	
-	public StatisticsResource(EventEditionRepository eventEditionRepo, StatisticsService statsService) {
+	public StatisticsResource(EventEditionRepository eventEditionRepo, SeriesEditionRepository seriesEditionRepo, 
+			SeriesEditionService seriesEditionServ, StatisticsService statsService) {
 		this.eventEditionRepo = eventEditionRepo;
+		this.seriesEditionRepo = seriesEditionRepo;
+		this.seriesEditionService = seriesEditionServ;
 		this.statsService = statsService;
 	}
 	
@@ -38,10 +47,13 @@ public class StatisticsResource {
 		statsService.deleteAllStatistics();
 		eventEditionRepo.findAllByOrderByEventDateAsc().stream()
 			.forEach(event -> statsService.buildEventStatistics(event));
-//		log.info("Series based ones...");
-//		seriesEditionRepo.findAll().stream()
-//			.forEach(series -> statsService.buildSeriesStatistics(series));
 		log.info("Statistics rebuilt...");
+		
+		seriesEditionRepo.findAll().stream()
+			.forEach(s -> {
+				List<Long> ids = s.getDriversChampions().stream().map(d -> d.getId()).collect(Collectors.toList());
+				seriesEditionService.setSeriesDriversChampions(s.getId(), ids);
+			});
 		return CompletableFuture.completedFuture(ResponseEntity.ok().headers(HeaderUtil.createAlert("Statistics rebuilt", null)).build());
 	}
 	
