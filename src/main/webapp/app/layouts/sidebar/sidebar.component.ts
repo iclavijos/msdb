02c77d3bacbaf93ccec04768e4ee56c6c777d4fa
Ivan Clavijos <ivan.clavijos@sngular.com>
2019-12-29 12:50:52 -0500

    Improvements over menu generation

--- src/main/webapp/app/layouts/sidebar/sidebar.component.ts
+++ src/main/webapp/app/layouts/sidebar/sidebar.component.ts
@@ -5,13 +5,19 @@ declare interface RouteInfo {
   path: string;
   title: string;
   icon: string;
+  iconClass: string;
   class: string;
 }
 export const ROUTES: RouteInfo[] = [
-  { path: '/driver', title: 'Driver', icon: 'person', class: '' },
-  { path: '/user-profile', title: 'Race track', icon: 'person', class: '' },
-  { path: '/table-list', title: 'Team', icon: 'content_paste', class: '' },
-  { path: '/typography', title: 'Engine', icon: 'library_books', class: '' }
+  { path: '/driver', title: 'global.menu.entities.driver', icon: '', iconClass: 'icofont icofont-racer', class: '' },
+  { path: '/racetrack', title: 'global.menu.entities.racetrack', icon: '', iconClass: 'icofont icofont-whistle', class: '' },
+  { path: '/team', title: 'global.menu.entities.team', icon: '', iconClass: 'icofont icofont-workers-group', class: '' },
+  { path: '/engine', title: 'global.menu.entities.engine', icon: '', iconClass: 'icofont icofont-whistle', class: '' },
+  { path: '/chassis', title: 'global.menu.entities.chassis', icon: '', iconClass: 'icofont icofont-racing-car', class: '' },
+  { path: '/tyre-provider', title: 'global.menu.entities.tyreProvider', icon: '', iconClass: 'icofont icofont-lemon-alt', class: '' },
+  { path: '/fuel-provider', title: 'global.menu.entities.fuelProvider', icon: '', iconClass: 'icofont icofont-glue-oil', class: '' },
+  { path: '/event', title: 'global.menu.entities.event', icon: '', iconClass: 'icofont icofont-racing-flag-alt', class: '' },
+  { path: '/series', title: 'global.menu.entities.series', icon: '', iconClass: 'icofont icofont-trophy', class: '' }
 ];
 
 @Component({
