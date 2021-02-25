cffa429cd3b2b836bcf4b4d9ba8c7575d57cdccc
Ivan Clavijos <ivan.clavijos@sngular.com>
2021-02-24 19:30:59 -0500

    Fixed initialization error when app reloaded (only applies to local development)

--- src/main/java/com/icesoft/msdb/config/FirebaseConfiguration.java
+++ src/main/java/com/icesoft/msdb/config/FirebaseConfiguration.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -11,6 +12,7 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.IOException;
 
 @Configuration
+@Slf4j
 public class FirebaseConfiguration {
 
     @Bean
@@ -22,10 +24,12 @@ public class FirebaseConfiguration {
             .setCredentials(googleCredentials)
             .build();
         FirebaseApp app = null;
-        if(FirebaseApp.getApps().isEmpty()) {
+        if (FirebaseApp.getApps().isEmpty()) {
+            log.debug("Initializing Firebase as apps list is empty");
             app = FirebaseApp.initializeApp(firebaseOptions, "com.icesoft.msdb.client");
-        }else {
-            app = FirebaseApp.initializeApp(firebaseOptions);
+        } else {
+            log.debug("Initializing Firebase with {} apps in the list", FirebaseApp.getApps().size());
+            app = FirebaseApp.getApps().get(0);
         }
         return FirebaseMessaging.getInstance(app);
     }
