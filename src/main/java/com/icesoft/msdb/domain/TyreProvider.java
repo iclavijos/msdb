d9a033c636cb59b49828c0c4b6801d99d74c1528
Ivan Clavijos <ivan.clavijos@sngular.com>
2022-01-19 22:42:17 -0500

    Added Lombok

--- src/main/java/com/icesoft/msdb/domain/TyreProvider.java
+++ src/main/java/com/icesoft/msdb/domain/TyreProvider.java
@@ -3,6 +3,8 @@ package com.icesoft.msdb.domain;
 import javax.persistence.*;
 import javax.validation.constraints.*;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
@@ -17,6 +19,7 @@ import java.util.Objects;
 @Table(name = "tyre_provider")
 @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 @Document(indexName = "tyreprovider")
+@Data @EqualsAndHashCode(callSuper = false)
 public class TyreProvider extends AbstractAuditingEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -44,30 +47,9 @@ public class TyreProvider extends AbstractAuditingEntity implements Serializable
     @Column(name="background_color")
     private String backgroundColor;
 
-    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
     public TyreProvider name(String name) {
         this.name = name;
         return this;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public byte[] getLogo() {
-        return logo;
     }
 
     public TyreProvider logo(byte[] logo) {
@@ -75,62 +57,9 @@ public class TyreProvider extends AbstractAuditingEntity implements Serializable
         return this;
     }
 
-    public void setLogo(byte[] logo) {
-        this.logo = logo;
-    }
-
-    public String getLogoUrl() {
-        return logoUrl;
-    }
-
     public TyreProvider logoUrl(String logoUrl) {
         this.logoUrl = logoUrl;
         return this;
     }
 
-    public void setLogoUrl(String logoUrl) {
-        this.logoUrl = logoUrl;
-    }
-
-    public String getLetterColor() {
-        return letterColor;
-    }
-
-    public void setLetterColor(String letterColor) {
-        this.letterColor = letterColor;
-    }
-
-    public String getBackgroundColor() {
-        return backgroundColor;
-    }
-
-    public void setBackgroundColor(String backgroundColor) {
-        this.backgroundColor = backgroundColor;
-    }
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof TyreProvider)) {
-            return false;
-        }
-        return id != null && id.equals(((TyreProvider) o).id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(getId());
-    }
-
-    @Override
-    public String toString() {
-        return "TyreProvider{" +
-            "id=" + getId() +
-            ", name='" + getName() + "'" +
-            ", logoUrl='" + getLogoUrl() + "'" +
-            '}';
-    }
 }
