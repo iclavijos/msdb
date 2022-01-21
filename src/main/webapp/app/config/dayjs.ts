34c2f7b8b7a73c5d76c8e4a6dee4bc9f59834dd3
Ivan Clavijos <ivan.clavijos@sngular.com>
2022-01-21 12:31:31 -0500

    Added localized format plugin

--- src/main/webapp/app/config/dayjs.ts
+++ src/main/webapp/app/config/dayjs.ts
@@ -1,5 +1,6 @@
 import * as dayjs from 'dayjs';
 import * as customParseFormat from 'dayjs/plugin/customParseFormat';
+import * as localizedFormat from 'dayjs/plugin/localizedFormat';
 import * as duration from 'dayjs/plugin/duration';
 import * as relativeTime from 'dayjs/plugin/relativeTime';
 
@@ -10,5 +11,6 @@ import 'dayjs/locale/es';
 
 // DAYJS CONFIGURATION
 dayjs.extend(customParseFormat);
+dayjs.extend(localizedFormat);
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
