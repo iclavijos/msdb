b181bbb34c7774f168fce788e30b8cb2a056c1a7
Ivan Clavijos <ivan.clavijos@sngular.com>
2022-01-22 14:11:33 -0500

    Fixed teams sorting and pagination problems

--- src/main/webapp/app/entities/team/list/team.component.ts
+++ src/main/webapp/app/entities/team/list/team.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { HttpResponse } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
-import { combineLatest } from 'rxjs';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { SessionStorageService } from 'ngx-webstorage';
 
-import { ITeam } from '../team.model';
-
-import { ASC, DESC, ITEMS_PER_PAGE, SORT } from 'app/config/pagination.constants';
+import { ASC, DESC, ITEMS_PER_PAGE } from 'app/config/pagination.constants';
+import { ITeam, Team } from '../team.model';
 import { TeamService } from '../service/team.service';
 import { TeamDeleteDialogComponent } from '../delete/team-delete-dialog.component';
 import { DataUtils } from 'app/core/util/data-util.service';
 
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
-import { MatSort, Sort } from '@angular/material/sort';
+import { MatSort, Sort, MatSortHeader } from '@angular/material/sort';
 
 @Component({
   selector: 'jhi-team',
@@ -27,9 +28,12 @@ export class TeamComponent implements OnInit, AfterViewInit {
   page = 0;
   predicate = 'name';
   ascending = true;
+  reloadData = true;
 
   dataSource = new MatTableDataSource<ITeam>([]);
   displayedColumns: string[] = ['name', 'description', 'hqLocation', 'logo', 'buttons'];
+
+  teamsSearchTextChanged = new Subject<string>();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sorter!: MatSort;
@@ -39,7 +43,8 @@ export class TeamComponent implements OnInit, AfterViewInit {
     protected activatedRoute: ActivatedRoute,
     protected dataUtils: DataUtils,
     protected router: Router,
-    protected modalService: NgbModal
+    protected modalService: NgbModal,
+    private sessionStorageService: SessionStorageService
   ) {
     this.currentSearch = this.activatedRoute.snapshot.queryParams['search'] ?? '';
   }
@@ -49,14 +54,47 @@ export class TeamComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit(): void {
+    this.currentSearch = this.sessionStorageService.retrieve('teamSearch') ?? '';
+    this.page = this.sessionStorageService.retrieve('teamSearchPage') ?? 0;
+    this.itemsPerPage = this.sessionStorageService.retrieve('teamSearchItems') ?? ITEMS_PER_PAGE;
+    this.predicate = this.sessionStorageService.retrieve('teamSearchPredicate') ?? 'name';
+    this.ascending = this.sessionStorageService.retrieve('teamSearchAscending') ?? true;
+
     this.loadPage();
+
+    this.teamsSearchTextChanged
+      .pipe(debounceTime(300))
+      .subscribe(() => {
+        this.page = 0;
+        this.itemsPerPage = ITEMS_PER_PAGE;
+        this.predicate = 'name';
+        this.ascending = true;
+        this.loadPage();
+      });
   }
 
-  search(query?: string): void {
-    if (!query) {
+  search(query: string): void {
+    this.sessionStorageService.store('teamSearch', query);
+    if (query.length === 0) {
       this.currentSearch = '';
+      this.page = 0;
+      this.itemsPerPage = ITEMS_PER_PAGE;
+      this.predicate = 'name';
+      this.ascending = true;
+      this.loadPage();
+    } else {
+      if (query.length >= 3) {
+        this.teamsSearchTextChanged.next();
+      }
     }
-    this.loadPage();
+  }
+
+  clearSearch(): void {
+    this.sessionStorageService.clear('teamSearchPage');
+    this.sessionStorageService.clear('teamSearchItems');
+    this.sessionStorageService.clear('teamSearchPredicate');
+    this.sessionStorageService.clear('teamSearchAscending');
+    this.search('');
   }
 
   trackId(index: number, item: ITeam): number {
@@ -86,30 +124,47 @@ export class TeamComponent implements OnInit, AfterViewInit {
   pageChanged(event: PageEvent): void {
     this.page = event.pageIndex;
     this.itemsPerPage = event.pageSize;
+    this.sessionStorageService.store('teamSearchPage', this.page);
+    this.sessionStorageService.store('teamSearchItems', this.itemsPerPage);
     this.loadPage();
   }
 
   sortChanged(event: Sort): void {
     this.predicate = event.active;
     this.ascending = (event.direction as string) === 'asc';
-    this.page = 0;
+    this.sessionStorageService.store('teamSearchPredicate', this.predicate);
+    this.sessionStorageService.store('teamSearchAscending', this.ascending);
     this.loadPage();
+    if (this.reloadData) {
+      this.page = 0;
+      this.sessionStorageService.store('teamSearchPage', this.page);
+      this.paginator.pageIndex = 0;
+    }
   }
 
   protected loadPage(): void {
+    if (!this.reloadData) {
+      return;
+    }
+
+    this.isLoading = true;
     this.teamService
       .query({
         page: this.page,
         query: this.currentSearch,
         size: this.itemsPerPage,
-        sort: [`${this.predicate},${this.ascending ? ASC : DESC }`],
+        sort: this.sort(),
       })
       .subscribe(
         (res: HttpResponse<ITeam[]>) => {
           this.isLoading = false;
           this.totalItems = Number(res.headers.get('X-Total-Count'));
-          this.dataSource = new MatTableDataSource(res.body ?? []);
+          this.dataSource = new MatTableDataSource(this.instantiateResponseObjects(res.body as ITeam[]));
+          this.sorter.active = this.predicate;
+          this.sorter.start = this.ascending ? 'asc' : 'desc';
           this.dataSource.sort = this.sorter;
+          this.paginator.pageIndex = this.page;
+          this.setSort(this.predicate, this.ascending ? 'asc' : 'desc');
         },
         () => {
           this.isLoading = false;
@@ -119,25 +174,42 @@ export class TeamComponent implements OnInit, AfterViewInit {
   }
 
   protected sort(): string[] {
-    return [this.predicate + ',' + (this.ascending ? ASC : DESC)];
+    const sortPredicates = [`${this.predicate},${this.ascending ? ASC : DESC }`];
+    if (this.predicate !== 'id') {
+      sortPredicates.push(`id,${ASC}`);
+    }
+    return sortPredicates;
   }
 
-  protected handleNavigation(): void {
-    combineLatest([this.activatedRoute.data, this.activatedRoute.queryParamMap]).subscribe(([data, params]) => {
-      const page = params.get('page');
-      const pageNumber = +(page ?? 1);
-      const sort = (params.get(SORT) ?? data['defaultSort']).split(',');
-      const predicate = sort[0];
-      const ascending = sort[1] === ASC;
-      if (pageNumber !== this.page || predicate !== this.predicate || ascending !== this.ascending) {
-        this.predicate = predicate;
-        this.ascending = ascending;
-        // this.loadPage(pageNumber, true);
-      }
-    });
+  private instantiateResponseObjects(data: ITeam[]): ITeam[] {
+    const objects: ITeam[] = [];
+    data.forEach(team => objects.push(
+      new Team(
+        team.id,
+        team.name,
+        team.description,
+        team.hqLocation,
+        null,
+        null,
+        team.logoUrl
+      )));
+
+    return objects;
   }
 
-//   protected onError(): void {
-//     this.page = this.page ?? 0;
-//   }
+  private setSort(id: string, start?: 'asc' | 'desc'): void {
+    start = start ?? 'asc';
+    const matSort = this.dataSource.sort;
+    const toState = 'active';
+    const disableClear = false;
+
+    // reset state so that start is the first sort direction that you will see
+    this.reloadData = false;
+    matSort!.sort({ id: '', start, disableClear });
+    matSort!.sort({ id, start, disableClear });
+    this.reloadData = true;
+
+    // ugly hack
+    (matSort!.sortables.get(id) as MatSortHeader)._setAnimationTransitionState({ toState });
+  }
 }
