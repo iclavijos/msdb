eadd064dd96e46b78c759fa52496f7c1d87fd453
Ivan Clavijos <ivan.clavijos@sngular.com>
2022-01-27 14:36:52 -0500

    MariaDB changes

--- src/main/resources/db/migration/V5_3_0__Migration_subscriptions_to_series.sql
+++ src/main/resources/db/migration/V5_3_0__Migration_subscriptions_to_series.sql
@@ -1,12 +1,12 @@
 ALTER TABLE user_suscription
-DROP FOREIGN KEY fk_series_suscription;
+DROP FOREIGN KEY IF EXISTS fk_series_suscription;
 
 DELETE FROM user_suscription WHERE user_id <> '1';
 
 ALTER TABLE user_suscription
 CHANGE COLUMN series_edition_id series_id BIGINT NOT NULL,
-DROP INDEX fk_series_suscription,
-ADD INDEX fk_series_suscription_idx1 (series_id ASC) VISIBLE;
+DROP INDEX IF EXISTS fk_series_suscription,
+ADD INDEX fk_series_suscription_idx1 (series_id ASC);
 
 ALTER TABLE user_suscription
 ADD CONSTRAINT fk_series_suscription
