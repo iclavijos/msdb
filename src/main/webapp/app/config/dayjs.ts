ff4351f88249121ed4b9d134287f615b893d849b
Ivan Clavijos <ivan.clavijos@sngular.com>
2022-02-03 16:45:28 -0500

    Added extra plugins

--- src/main/webapp/app/config/dayjs.ts
+++ src/main/webapp/app/config/dayjs.ts
@@ -4,6 +4,7 @@ import * as localizedFormat from 'dayjs/plugin/localizedFormat';
 import * as duration from 'dayjs/plugin/duration';
 import * as relativeTime from 'dayjs/plugin/relativeTime';
 import * as timezone from 'dayjs/plugin/timezone';
+import * as utc from "dayjs/plugin/utc";
 
 // jhipster-needle-i18n-language-dayjs-imports - JHipster will import languages from dayjs here
 import 'dayjs/locale/en';
@@ -11,8 +12,9 @@ import 'dayjs/locale/ca';
 import 'dayjs/locale/es';
 
 // DAYJS CONFIGURATION
+dayjs.extend(utc);
+dayjs.extend(timezone);
 dayjs.extend(customParseFormat);
 dayjs.extend(localizedFormat);
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
-dayjs.extend(timezone);
