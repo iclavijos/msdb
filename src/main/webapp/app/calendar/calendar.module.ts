5a796a5390b968039491f426e18d3da98b17a763
Ivan Clavijos <ivan.clavijos@sngular.com>
2022-02-10 09:09:44 -0500

    Calendar component migration

--- src/main/webapp/app/calendar/calendar.module.ts
+++ src/main/webapp/app/calendar/calendar.module.ts
@@ -2,9 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import timeLinePlugin from '@fullcalendar/timeline';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
+import luxon2Plugin from '@fullcalendar/luxon2';
+import bootstrap5Plugin from '@fullcalendar/bootstrap5';
+
 import { CalendarComponent, EventDialogComponent } from './calendar.component';
 
 import { calendarRoute } from './calendar.route';
+
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  timeGridPlugin,
+  timeLinePlugin,
+  listPlugin,
+  interactionPlugin,
+  luxon2Plugin,
+  bootstrap5Plugin
+]);
 
 @NgModule({
   imports: [SharedModule, RouterModule.forChild([calendarRoute])],
