5ffd6e4f1ec35e734f2e763b53ca6b38707e71e2
Ivan Clavijos <ivan.clavijos@sngular.com>
2022-02-13 13:55:05 -0500

    Header upgrade - initial iteration

---
+++ src/main/webapp/app/layouts/header/header.component.ts
@@ -0,0 +1,174 @@
+import { DOCUMENT } from '@angular/common';
+import {
+  Component,
+  Inject,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  AfterViewInit,
+} from '@angular/core';
+import { Router } from '@angular/router';
+
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { TranslateService } from '@ngx-translate/core';
+import { SessionStorageService } from 'ngx-webstorage';
+
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+import { VERSION } from 'app/app.constants';
+import { LANGUAGES } from 'app/config/language.constants';
+import { Account } from 'app/core/auth/account.model';
+import { AccountService } from 'app/core/auth/account.service';
+import { LoginService } from 'app/login/login.service';
+import { ProfileService } from 'app/layouts/profiles/profile.service';
+import { ConfigService } from 'app/config/config.service';
+import { FullScreenService } from 'app/shared/services/fullscreen.service';
+
+
+@Component({
+  selector: 'jhi-app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.sass'],
+})
+export class HeaderComponent implements OnInit, AfterViewInit {
+  public config: any = {};
+  inProduction?: boolean;
+  isNavbarCollapsed = true;
+  languages = LANGUAGES;
+  openAPIEnabled?: boolean;
+  version = '';
+  account: Account | null = null;
+  userFullName = '';
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2,
+    public elementRef: ElementRef,
+    private loginService: LoginService,
+    private translateService: TranslateService,
+    private sessionStorageService: SessionStorageService,
+    private accountService: AccountService,
+    private profileService: ProfileService,
+    private configService: ConfigService,
+    private fullScreenService: FullScreenService,
+    // private authService: AuthService,
+    public router: Router
+  ) {
+    if (VERSION) {
+      this.version = VERSION.toLowerCase().startsWith('v') ? VERSION : `v${VERSION}`;
+    }
+  }
+
+  ngOnInit(): void {
+    this.config = this.configService.configData;
+    this.profileService.getProfileInfo().subscribe(profileInfo => {
+      this.inProduction = profileInfo.inProduction;
+      this.openAPIEnabled = profileInfo.openAPIEnabled;
+    });
+    this.accountService.getAuthenticationState()
+      .subscribe(
+        account =>
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+          (this.account = account));
+  }
+
+  ngAfterViewInit() {
+    // set theme on startup
+    if (localStorage.getItem('theme')) {
+      this.renderer.removeClass(this.document.body, this.config.layout.variant);
+      this.renderer.addClass(this.document.body, localStorage.getItem('theme') ?? 'light');
+    } else {
+      this.renderer.addClass(this.document.body, this.config.layout.variant);
+    }
+
+    if (localStorage.getItem('menuOption')) {
+      this.renderer.addClass(
+        this.document.body,
+        localStorage.getItem('menuOption') ?? 'light'
+      );
+    } else {
+      this.renderer.addClass(
+        this.document.body,
+        `menu-${this.config.layout.sidebar.backgroundColor}`
+      );
+    }
+
+    if (localStorage.getItem('choose_logoheader')) {
+      this.renderer.addClass(
+        this.document.body,
+        localStorage.getItem('choose_logoheader') ?? 'white'
+      );
+    } else {
+      this.renderer.addClass(
+        this.document.body,
+        `logo-${this.config.layout.logo_bg_color}`
+      );
+    }
+
+    if (localStorage.getItem('sidebar_status')) {
+      if (localStorage.getItem('sidebar_status') === 'close') {
+        this.renderer.addClass(this.document.body, 'side-closed');
+        this.renderer.addClass(this.document.body, 'submenu-closed');
+      } else {
+        this.renderer.removeClass(this.document.body, 'side-closed');
+        this.renderer.removeClass(this.document.body, 'submenu-closed');
+      }
+    } else {
+      if (this.config.layout.sidebar.collapsed === true) {
+        this.renderer.addClass(this.document.body, 'side-closed');
+        this.renderer.addClass(this.document.body, 'submenu-closed');
+      }
+    }
+  }
+
+  changeLanguage(languageKey: string): void {
+    this.sessionStorageService.store('locale', languageKey);
+    this.translateService.use(languageKey);
+  }
+
+  isAuthenticated(): boolean {
+    const authenticated = this.accountService.isAuthenticated();
+    if (authenticated) {
+      this.accountService.identity().subscribe(userIdentity => (this.userFullName = `${userIdentity?.firstName} ${userIdentity?.lastName}`));
+    }
+    return authenticated;
+  }
+
+  login(): void {
+    this.loginService.login();
+  }
+
+  logout(): void {
+    this.loginService.logout();
+    this.router.navigate(['']);
+  }
+
+  callFullscreen(): void {
+    this.fullScreenService.toggle();
+  }
+
+  mobileMenuSidebarOpen(event: any, className: string): void {
+    const hasClass = event.target.classList.contains(className);
+    if (hasClass) {
+      this.renderer.removeClass(this.document.body, className);
+    } else {
+      this.renderer.addClass(this.document.body, className);
+    }
+  }
+
+  callSidemenuCollapse(): void {
+    const hasClass = this.document.body.classList.contains('side-closed');
+    if (hasClass) {
+      this.renderer.removeClass(this.document.body, 'side-closed');
+      this.renderer.removeClass(this.document.body, 'submenu-closed');
+    } else {
+      this.renderer.addClass(this.document.body, 'side-closed');
+      this.renderer.addClass(this.document.body, 'submenu-closed');
+    }
+  }
+
+  getImageUrl(): string | null {
+    return this.isAuthenticated() ? this.accountService.getImageUrl() : null;
+  }
+}
