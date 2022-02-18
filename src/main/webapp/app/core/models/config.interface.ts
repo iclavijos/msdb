91084f91ae457105a8cf5c59ec6c6dfcecdf40b9
Ivan Clavijos <ivan.clavijos@sngular.com>
2022-02-18 16:05:19 -0500

    Config service for UI data

---
+++ src/main/webapp/app/core/models/config.interface.ts
@@ -0,0 +1,12 @@
+export interface InConfiguration {
+  layout: {
+    rtl: boolean;
+    variant: string;
+    theme_color: string;
+    logo_bg_color: string;
+    sidebar: {
+      collapsed: boolean;
+      backgroundColor: string;
+    };
+  };
+}
