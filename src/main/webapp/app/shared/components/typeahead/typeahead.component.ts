9d38453f2eccbad11f073becbc6d98762b0244c6
Ivan Clavijos <ivan.clavijos@sngular.com>
2022-02-22 13:51:53 -0500

    New typeahead component

---
+++ src/main/webapp/app/shared/components/typeahead/typeahead.component.ts
@@ -0,0 +1,95 @@
+import { Component, Input, ElementRef, OnInit } from '@angular/core';
+import { FormGroup } from "@angular/forms";
+
+import { Observable, BehaviorSubject } from 'rxjs';
+import { takeWhile, tap } from 'rxjs/operators';
+
+export type DisplayWithFunc = <T>(item: T) => string;
+export type TooltipFunc = <T>(item: T) => string;
+
+@Component({
+  selector: 'jhi-autocomplete',
+  templateUrl: './typeahead.component.html'
+})
+export class TypeAheadComponent<T> implements OnInit {
+
+  @Input() editForm!: FormGroup;
+  @Input() elementId!: string;
+  @Input() controlName!: string;
+  @Input() label!: string;
+  @Input() items!: Observable<T[] | null>;
+  @Input() isRequired = false;
+  @Input() imageAttr = '';
+  @Input() imageUrlPattern!: string;
+  @Input() icon = '';
+
+  @Input() displayWithFunction!: DisplayWithFunc;
+  @Input() tooltipFunction!: TooltipFunc;
+
+  private typeAheadBlurred = new BehaviorSubject(false);
+  private typeAheadValueSelected = new BehaviorSubject(false);
+  private isTimerStarted = false;
+  private isValueSelected = false;
+  private isValueChanged = false;
+  private isAlive = true;
+  private CHECK_SELECTION_TIMEOUT = 50;
+  private timer!: any;
+
+  constructor(
+    private elementRef: ElementRef
+  ) {}
+
+  ngOnInit(): void {
+    this.typeAheadBlurred.pipe(
+      takeWhile(() => this.isAlive), // auto-unsubscribe
+      tap((next) => {
+        this.isTimerStarted = next;
+        if (this.isTimerStarted) {
+          clearTimeout(this.timer);
+          this.timer = setTimeout(() => {
+              if (!this.isValueSelected && this.isValueChanged) {
+                this.elementRef.nativeElement.querySelector('#' + this.elementId).value = null;
+                this.editForm.patchValue({
+                  [this.controlName]: null,
+                });
+                this.isValueChanged = false;
+              }
+            this.isValueSelected = false;
+            this.typeAheadBlurred.next(false);
+          }, this.CHECK_SELECTION_TIMEOUT);
+        }
+      })
+      ).subscribe();
+
+    this.typeAheadValueSelected.pipe(
+      takeWhile(() => this.isAlive)
+    ).subscribe((valueSelected) => this.isValueSelected = valueSelected);
+  }
+
+  onValueChanged(): void {
+    this.isValueChanged = true;
+  }
+
+  onBlurLayout(): void {
+    this.typeAheadBlurred.next(true);
+  }
+
+  onOptionSelected(): void {
+    this.isValueSelected = true;
+    this.typeAheadValueSelected.next(true);
+  }
+
+  isImageValueAvailable(item: T): boolean {
+    return (item as any)[this.imageAttr] !== undefined && (item as any)[this.imageAttr] !== '';
+  }
+
+  getImageSrc(item: T): string {
+    const imageName = (item as any)[this.imageAttr] as string;
+    if (this.imageUrlPattern) {
+      return this.imageUrlPattern.replace('%s', imageName);
+    } else {
+      return imageName;
+    }
+  }
+
+}
