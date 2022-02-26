2eecb7ba7623bcadfeb14fe79c74d33f97eee611
Ivan Clavijos <ivan.clavijos@sngular.com>
2022-02-26 10:49:23 -0500

    Breadcrumb homogenization

--- webpack/webpack.custom.js
+++ webpack/webpack.custom.js
@@ -105,7 +105,8 @@ module.exports = async (config, options, targetOptions) => {
     { from: './src/main/webapp/content/images', to: 'images' },
     { from: './node_modules/leaflet/dist/images', to: 'assets'},
     // jhipster-needle-add-assets-to-webpack - JHipster will add/remove third-party resources in this array
-    { from: './src/main/webapp/robots.txt', to: 'robots.txt' }
+    { from: './src/main/webapp/robots.txt', to: 'robots.txt' },
+    { from: '/tmp/maintenance.txt', to: 'maintenance.txt'}
   ];
 
   if (patterns.length > 0) {
