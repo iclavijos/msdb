4825325fe1f6fa9ab40a8aec54edc0eaf5b87a37
Ivan Clavijos <ivan.clavijos@sngular.com>
2022-07-27 09:58:58 -0400

    Improvement to duration type handling

--- src/main/java/com/icesoft/msdb/domain/enums/DurationType.java
+++ src/main/java/com/icesoft/msdb/domain/enums/DurationType.java
@@ -1,12 +1,39 @@
 package com.icesoft.msdb.domain.enums;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum DurationType {
-	MINUTES,
-	HOURS,
-	KMS,
-	MILES,
-	LAPS;
+	MINUTES("minutes"),
+	HOURS("hours"),
+	KMS("kms"),
+	MILES("miles"),
+	LAPS("laps");
+
+    private final String desc;
+
+    DurationType(String desc) {
+        this.desc = desc;
+    }
+
+    @JsonValue
+    public String toString() {
+        return desc;
+    }
+
+    public static DurationType fromDesc(String desc) {
+        switch (desc) {
+            case "minutes":
+                return MINUTES;
+            case "hours":
+                return HOURS;
+            case "kms":
+                return KMS;
+            case "miles":
+                return MILES;
+            case "laps":
+                return LAPS;
+            default:
+                throw new IllegalArgumentException(desc + " is not a valid DurationType");
+        }
+    }
 }
--- src/main/resources/db/migration/V5_5_0_1__Fix_to_event_session_type_enums.sql
+++ src/main/resources/db/migration/V5_5_0_1__Fix_to_event_session_type_enums.sql
@@ -1,3 +1,5 @@
 update event_session set session_type = lower(session_type);
+update event_session set duration_type = lower(duration_type);
 
+update event_session set session_type = 'qualifyingRace' where session_type = 'qualifying_race';
 update event_session set session_type = 'qualifyingRace' where session_type = 'qualifyingrace';
