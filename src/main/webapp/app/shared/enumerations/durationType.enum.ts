43503c84724422611f5d71147badac13631a0830
Ivan Clavijos <ivan.clavijos@sngular.com>
2022-07-27 10:06:13 -0400

    Added string values

--- src/main/webapp/app/shared/enumerations/durationType.enum.ts
+++ src/main/webapp/app/shared/enumerations/durationType.enum.ts
@@ -1,7 +1,7 @@
 export enum DurationType {
-  MINUTES,
-  HOURS,
-  KILOMETERS,
-  MILES,
-  LAPS
+  MINUTES = 'minutes',
+  HOURS = 'hours',
+  KILOMETERS = 'kms',
+  MILES = 'miles',
+  LAPS = 'laps'
 }
