96d00aa641721112d7381487e4c66ba8a4fb634c
Ivan Clavijos <ivan.clavijos@sngular.com>
2022-08-18 16:26:21 -0400

    Updating view to use proper column name

--- src/main/resources/db/migration/V5_5_0_3__Migrate_session_start_to_timestamp_type.sql
+++ src/main/resources/db/migration/V5_5_0_3__Migrate_session_start_to_timestamp_type.sql
@@ -7,3 +7,32 @@ WHERE start_time_ts <> 0;
 
 ALTER TABLE event_session
 DROP COLUMN start_time_ts;
+
+CREATE OR REPLACE
+    ALGORITHM = UNDEFINED
+    DEFINER = `msdb`@`localhost`
+    SQL SECURITY DEFINER
+VIEW `events_results` AS
+    SELECT
+        `ed`.`id` AS `editionId`,
+        `ee`.`id` AS `entryId`,
+        `ee`.`team_name` AS `teamName`,
+        `c`.`id` AS `catId`,
+        `c`.`shortname` AS `catName`,
+        `es`.`name` AS `sessionName`,
+        `eer`.`final_position` AS `finalPos`,
+        `es`.`start_time` AS `sessionStartTime`
+    FROM
+        ((((`event_entry_result` `eer`
+        JOIN `event_entry` `ee`)
+        JOIN `event_session` `es`)
+        JOIN `event_edition` `ed`)
+        JOIN `category` `c`)
+    WHERE
+        `eer`.`session_id` = `es`.`id`
+            AND `es`.`event_edition_id` = `ed`.`id`
+            AND (`es`.`session_type` = 'race'
+            OR `es`.`session_type` = 'qualifyingRace')
+            AND `eer`.`entry_id` = `ee`.`id`
+            AND `ee`.`category_id` = `c`.`id`
+    ORDER BY `es`.`start_time`
