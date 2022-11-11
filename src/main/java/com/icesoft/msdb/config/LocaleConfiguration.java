14c797032e380f04ed7267d8fbcb581c93c9fd7f
Ivan Clavijos <ivan.clavijos@sngular.com>
2022-11-11 11:49:13 -0500

    Deprecated method fix

--- src/main/java/com/icesoft/msdb/config/LocaleConfiguration.java
+++ src/main/java/com/icesoft/msdb/config/LocaleConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import tech.jhipster.config.locale.AngularCookieLocaleResolver;
 
@@ -12,8 +13,7 @@ public class LocaleConfiguration implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        AngularCookieLocaleResolver cookieLocaleResolver = new AngularCookieLocaleResolver();
-        cookieLocaleResolver.setCookieName("NG_TRANSLATE_LANG_KEY");
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver("NG_TRANSLATE_LANG_KEY");
         return cookieLocaleResolver;
     }
 
